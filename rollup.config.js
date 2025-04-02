de87590b056b6d03c4cc09108570c1ae4575e853
Abduvali Karimov <dev.karimov@gmail.com>
2025-04-02 09:05:28 +0500

    chore: fix deps updates

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
-import externals from "rollup-plugin-node-externals";
+import { nodeExternals } from "rollup-plugin-node-externals";
 import json from "@rollup/plugin-json";
 import replace from "@rollup/plugin-replace";
 import resolve from "@rollup/plugin-node-resolve";
@@ -21,7 +21,7 @@ export default [
       { format: "es", file: pkg.module },
     ],
     plugins: [
-      externals({ deps: true }),
+      nodeExternals({ deps: true }),
       replace({ "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV) }),
       json(),
       resolve({ preferBuiltins: true, extensions }),
@@ -34,6 +34,6 @@ export default [
   {
     input: "src/index.ts",
     output: { format: "es", file: pkg.types },
-    plugins: [externals({ deps: true }), dts({ respectExternal: true })],
+    plugins: [nodeExternals({ deps: true }), dts({ respectExternal: true })],
   },
 ];
